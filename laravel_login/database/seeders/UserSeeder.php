239f8fe0022789550c0f3a348bcf73fe53a4eea9
asrobika01 <asrobika01@gmail.com>
2022-03-30 21:50:05 +0700

    Praktikum 2: Add login as Administrator

--- laravel_login/database/seeders/UserSeeder.php
+++ laravel_login/database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+           'name' => 'Nurlaily Asrobika',
+           'email'=> 'asrobika01@gmail.com',
+           'password'=> Hash::make('password'),
+        ]);
     }
 }
+
